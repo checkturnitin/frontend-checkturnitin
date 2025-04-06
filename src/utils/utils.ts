b0fa99c189b23a42c1739f6c8dac07d2a8f745ad
Sunab <67991569+sypase@users.noreply.github.com>
2025-04-06 09:38:51 +0545

    asd

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,18 +1,18 @@
 // const serverURL = "https://116.203.47.54:8080/"
 // const serverURL = "https://noaigptserver.xyz"
 // const serverURL = "https://eb70-27-34-65-119.ngrok-free.app"
-// const serverURL = "http://localhost:8080"
-const serverURL = "https://api.checkturnitin.com"
+// const serverURL = "http://localhost:8081"
+const serverURL = "https://api.aiplagreport.com"
 // const serverURL = "https://api.checkturnitin.com"
 // const serverURL = "https://api.checkturnitin.com"
 // const serverURL = "https://b0fe-2400-1a00-b030-cee6-b979-df81-c2f5-54ab.ngrok-free.app"
 // const serverURL = "http://192.168.1.90:8080"
 // const serverURL = "https://development.noaigpt.com"
 
-const frontendURL = "https://checkturnitin.com";
+const frontendURL = "https://aiplagreport.com";
 
 const currencySymbol = "₹";
-const appName = "Checkturnitin";
+const appName = "aiplagreport";
 const stripeKey = "pk_test_SCTPV4fZ9kMlryTRZ6XXzgDac90D00AQLTb51NaV7yFrkiQqJuN9j1IlzllFXnYSyCU1BVYpyXxBzycVuM5jTVFKB5FauiR3c5";
 
 
