b4d497d66e6271bd59d304cf5ecf14e23427e0f2
Ranjitkar <67991569+sypase@users.noreply.github.com>
2025-10-16 14:25:44 +0545

    asd

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -708,7 +708,7 @@ export default function Home() {
                         <p className="text-sm sm:text-base text-gray-900 dark:text-white font-medium">
                           Join our Discord channel and get{" "}
                           <span className="text-green-600 dark:text-green-400 font-bold">
-                            2 credits
+                            1 credits
                           </span>{" "}
                           as a bonus!
                         </p>
